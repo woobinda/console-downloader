2a7f0f562c455043f769ec19f618a293545c9804
woobinda <woobinda@voliacable.com>
2016-11-25 21:05:18 +0200

    add BANDWIDTH

--- console-utility/main.py
+++ console-utility/main.py
@@ -3,8 +3,9 @@ import threading
 import urllib.request
 import time
 
+from time import sleep
+from datetime import datetime, timedelta
 from multiprocessing import Queue
-from settings import *
 
 
 queue_lock = threading.Lock()
@@ -16,18 +17,21 @@ class DowloadWorker(threading.Thread):
 
         queue           - array of URLs for download
         queue_lock      - threading Lock object
+        BANDWIDTH       - stream rate, kbit/s
     """
-    def __init__(self, queue, queue_lock):
+
+    def __init__(self, queue, queue_lock, BANDWIDTH):
         super(DowloadWorker, self).__init__()
         self.queue = queue
         self.queue_lock = queue_lock
+        self.BANDWIDTH = BANDWIDTH
 
     def run(self):
         while True:
             nexturl = self.grab_next_url()
             if nexturl is None:
                 break
-            self.download_file(nexturl[0], nexturl[1])
+            self.download_file(nexturl[0], nexturl[1], self.BANDWIDTH)
 
     def grab_next_url(self):
         """
@@ -41,50 +45,63 @@ class DowloadWorker(threading.Thread):
         self.queue_lock.release()
         return nexturl
 
-    def download_file(self, file_url, local_filename):
+    def download_file(self, file_url, local_filename, BANDWIDTH):
         """Downloading data from url to local folder.
 
             file_url            - link to download file
             local_filename      - filename for local recording
-            total_size          - total size of all downloads
         """
-        global total_size
+        global total_size   # total size of all downloads
+        SECOND = timedelta(seconds=1)
         file = os.path.join(UPLOAD_DIR + '/' + local_filename)
-        with urllib.request.urlopen(file_url) as data, \
-                open(file, 'wb') as filename:
-            bits = filename.write(data.read())
-        total_size += bits
+        with urllib.request.urlopen(file_url) as data:
+            with open(file, 'wb') as filename:
+                for chunk in iter(lambda: data.read(BANDWIDTH), b""):
+                    last_time = datetime.now()   # start time for writing chunk
+                    filename.write(chunk)
+                    time_passed = datetime.now() - last_time   # time delta
+                    if time_passed < SECOND:   # if the extra time - wait
+                        time.sleep((SECOND - time_passed).microseconds /
+                                   1000000.0)
+        size = os.path.getsize(file)
+        total_size += size
 
 
-def main(urls_list, threads_count):
+def main(urls_list, threads_count, total_BANDWIDTH):
     """Main process for downloading data from URL-addresses.
-    Reads a file and adds links in queue, init threads.
+    Reads a file and adds links in queue, init threads:
 
         urls_list         - file with array of URLs for download
         threads_count     - count of threads/workers
-        start             - starting process time
-        end               - ending process time
+        BANDWIDTH         - total stream rate for all threads, kbits/s
     """
     print("\nSTARTED...\n")
-    start = time.localtime()
+    start = time.localtime()   # starting process time
     with open(urls_list) as links:
         for url in links:
             queue.put(url.rstrip().split(' '))
 
+    BANDWIDTH = total_BANDWIDTH // threads_count   # rate for a single stream
     for i in range(threads_count):
-        thread = DowloadWorker(queue, queue_lock)
+        thread = DowloadWorker(queue, queue_lock, BANDWIDTH)
         thread.start()
     while threading.active_count() > 2:
         time.sleep(1)
 
-    print("...FINISHED")
-    end = time.localtime()
+    print("...FINISHED\n")
+    end = time.localtime()   # ending process time
+    end_mins = abs(end.tm_min - start.tm_min)
+    end_secs = abs(end.tm_sec - start.tm_sec)
     print('Summary downloading size : ' +
-          '%.1f mb (%s bytes)' % ((total_size / 100000), total_size))
-    print('Total downloading time: ' + '%s min %s sec \n' % (
-        (end.tm_min - start.tm_min), (end.tm_sec - start.tm_sec))
+          '%.1f mb (%s bytes)' % ((total_size / 1000000), total_size))
+    print('Total downloading time: ' + '%d min %d sec \n' % (
+        (end_mins), (end_secs))
     )
 
 
 if __name__ == '__main__':
-    main(urls_list, threads_count)
+
+    from settings import threads_count, urls_list, upload_folder, \
+        total_BANDWIDTH, total_size, UPLOAD_DIR
+
+    main(urls_list, threads_count, total_BANDWIDTH)
--- console-utility/settings.py
+++ console-utility/settings.py
@@ -2,21 +2,38 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n", type=int, default=5,
+parser.add_argument("-n", type=int, default=5,
                     help='Threads count'),
-parser.add_argument("--f", type=str, default='links.txt',
+parser.add_argument("-f", type=str, default='links.txt',
                     help='Path to links for download'),
-parser.add_argument("--o", type=str, default='uploads',
+parser.add_argument("-l", type=str, default='200k',
+                    help='Download rate limit')
+parser.add_argument("-o", type=str, default='uploads',
                     help='local folder for upload data')
 args = parser.parse_args()
 
 threads_count = args.n
 urls_list = args.f
 upload_folder = args.o
+total_BANDWIDTH = args.l
 total_size = 0
 
 
+def calculate_BANDWIDTH(total_BANDWIDTH):
+    """Calculating stream rate units,
+    depending on the transmitted from the console suffix.
+    """
+    if total_BANDWIDTH[-1] == 'k':
+        total_BANDWIDTH = int(total_BANDWIDTH[:-1]) * 1024
+    elif total_BANDWIDTH[-1] == 'm':
+        total_BANDWIDTH = int(total_BANDWIDTH[:-1]) * 1024 * 1024
+    return total_BANDWIDTH
+
+total_BANDWIDTH = calculate_BANDWIDTH(total_BANDWIDTH)
+
+
 BASE_DIR = os.path.abspath('.')
+
 if not os.path.exists(os.path.join(BASE_DIR,
                                    upload_folder)):
     os.mkdir(upload_folder)
